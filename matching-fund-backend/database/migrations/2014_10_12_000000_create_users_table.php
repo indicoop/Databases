acd0262084d377c7532e4b73d8babfb6258b0dfe
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2022-04-19 20:53:22 +0700

    update user

--- matching-fund-backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ matching-fund-backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role_id');
+            $table->string('credit_card_number');
+            $table->string('phone_number');
+            $table->string('gender');
+            $table->text('address');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
